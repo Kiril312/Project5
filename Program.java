d0eca85b8315b02e31720e170590182ceed5cf79
Kiril <pelka.vsg@gmail.com>
2017-01-16 11:26:39 +0200

    multik

--- Program.java
+++ Program.java
@@ -1,12 +1,36 @@
+import java.util.Scanner;
+
 public class Program
 {
+	Scanner scanner = new Scanner (System.in);
+	
 	public static void main(String [] args)
 	{
 		System.out.print("Hello");
+		Program program = new Program ();
+		program.start ();
+	}
+	
+	public void start () 
+	{
+		int a = input ("First number : ");
+		int b = input ("Second number: ");
+		System.out.println ("sum of numbers: " + sum (a, b));
+	}
+	
+	public int input (String text)
+	{
+		System.out.print (text + ": ");
+		return scanner.nextInt ();
 	}
 		
 		public int sum (int a, int b)
 		{
 		return a + b;
+		}
+		
+		public int mul (int a, int b)
+		{
+			return a * b;
 		}
 }
